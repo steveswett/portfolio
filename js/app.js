647f37d93ec6fc088c85ed4c5481f619831d4686
Steve Swett <sswett@thinkful.com>
2015-02-27 16:29:32 -0500

    Add manage design and page maker apps

--- js/app.js
+++ js/app.js
@@ -6,8 +6,37 @@ $(document).ready(function() {
 function doOnReady()
 {
 	$("#tennis-project-link").click(function() {
-		console.log("tennis project link clicked");
-		window.location.hash = 'tennisproject';   // Does not appear to like dashes in hash name
-		window.scrollBy(0, 0 - $(".navbar").height() - parseInt($("#tennisproject").css("margin-top")) ) ;
+
+		// Does not appear to like dashes in hash name.  For some reason it couldn't distinguish between
+		// "tennis-project" and "tennis-project-link".
+
+		jumpToProjectSection('tennisproject');   
 	})
+
+	$("#bb-engage-project-link").click(function() {
+		jumpToProjectSection('bbengageproject');
+	})
+
+	$("#page-maker-project-link").click(function() {
+		jumpToProjectSection('pagemakerproject');
+	})
+}
+
+
+function jumpToProjectSection(pProjectSectionId)
+{
+		// var bodyScrollTop = $("body").scrollTop();
+		window.location.hash = pProjectSectionId;
+
+		// TODO: problem that may need solving (but is tricky): clicking project nav while project already visible ends up
+		// executing the "scrollBy" below.  Might not be easy to bypass.  Scroll is there due to "fixed" nav header.
+
+		// console.log("bodyScrollTop = " + bodyScrollTop);
+
+		// if (bodyScrollTop == 0)
+		// {
+
+			window.scrollBy(0, 0 - $(".navbar").height() - parseInt($("#" + pProjectSectionId).css("margin-top")) ) ;
+
+		//}
 }
