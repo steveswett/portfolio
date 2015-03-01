08744e01b1bfdeba0126422fcbb699a5950c35c4
Steve Swett <sswett@thinkful.com>
2015-03-01 16:18:32 -0500

    Implement good fix for hash link jumping when fixed header present

--- js/app.js
+++ js/app.js
@@ -7,44 +7,12 @@ function doOnReady()
 {
 	$("#ema").attr("href", "mail" + "to:" + "steve.swett" + "@" + "comcast.net");
 
-	$("#tennis-project-link").click(function() {
-
-		// Does not appear to like dashes in hash name.  For some reason it couldn't distinguish between
-		// "tennis-project" and "tennis-project-link".
-
-		jumpToProjectSection('tennisproject');   
-	});
-
-	$("#bb-engage-project-link").click(function() {
-		jumpToProjectSection('bbengageproject');
-	});
-
-	$("#page-maker-project-link").click(function() {
-		jumpToProjectSection('pagemakerproject');
-	});
-
-	$("#tennis-crud-project-link").click(function() {
-		jumpToProjectSection('tenniscrudproject');   
-	});
-
-	$("#java-app-samples-project-link").click(function() {
-		jumpToProjectSection('javaappsamplesproject');   
-	})
-
-	$("#ruby-app-samples-project-link").click(function() {
-		jumpToProjectSection('rubyappsamplesproject');   
-	});
-
-	$("#about-contact-link").click(function() {
-		jumpToProjectSection('aboutsection');   
+	$(".project-nav .glyphicon-chevron-left").click(function() {
+		jumpToAdjacentProjectSection(this, false);
 	});
 
 	$(".project-nav .glyphicon-chevron-right").click(function() {
 		jumpToAdjacentProjectSection(this, true);
-	});
-
-	$(".project-nav .glyphicon-chevron-left").click(function() {
-		jumpToAdjacentProjectSection(this, false);
 	});
 
 }
@@ -53,32 +21,12 @@ function doOnReady()
 function jumpToAdjacentProjectSection(pRightArrowElem, pNextTruePrevFalse)
 {
 	currentProjectSection = $(pRightArrowElem).closest(".project-section");
-	// console.log("currentProjectSectionId = " + currentProjectSectionId);
 
 	var adjacentProjectSection = pNextTruePrevFalse ? currentProjectSection.next(".project-section") : 
 		currentProjectSection.prev(".project-section");
 
 	if (adjacentProjectSection.length)
 	{
-		jumpToProjectSection(adjacentProjectSection.attr("id"));
+		window.location.hash = adjacentProjectSection.attr("id");
 	}
-}
-
-
-function jumpToProjectSection(pProjectSectionId)
-{
-	// var bodyScrollTop = $("body").scrollTop();
-	window.location.hash = pProjectSectionId;
-
-	// TODO: problem that may need solving (but is tricky): clicking project nav while project already visible ends up
-	// executing the "scrollBy" below.  Might not be easy to bypass.  Scroll is there due to "fixed" nav header.
-
-	// console.log("bodyScrollTop = " + bodyScrollTop);
-
-	// if (bodyScrollTop == 0)
-	// {
-
-		window.scrollBy(0, 0 - $(".navbar").height() - parseInt($("#" + pProjectSectionId).css("margin-top")) ) ;
-
-	//}
 }
